37ae22eba6cacea0713257a25256f63e52ef58f6
Losangelo <losangelo@gmail.com>
2020-04-18 22:15:59 -0300

    acomodação de parametro count para [count]

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,7 @@ const connection = require('../database/connection');
 module.exports = {
   async index(req, res){
     const {page = 1} = req.query;
-    const count = await connection('incidents').count();
+    const [count] = await connection('incidents').count();
  
     const incidents = await connection('incidents')
     .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
